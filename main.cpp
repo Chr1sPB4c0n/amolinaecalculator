ab0ea77f49c9c1a72ce5c3cdec369c68fb9f5e54
Alejandro <65742076+amolinae06@users.noreply.github.com>
2021-10-17 22:41:14 -0500

    added welcome screen and substract option.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 int main(){
+//Welcome, screen + my Github repository.
+std::cout << "Welcome to amolinae's calculator!\n";
+std::cout << "Github repository: https://github.com/amolinae06/amolinaecalculator\n";
+std::cout << "\n";
 
 //Variables, will be used later in the code.
 int num1 = 0;
 int num2 = 0;
 
 //Asks for to inputs num1 and num2.
-std::cout << "Welcome! \n";
 std::cout << "Enter a number: ";
 std::cin >> num1;
 std::cout << "You entered, " << num1 << "\n";
@@ -15,8 +18,20 @@ std::cout << "Enter another number: ";
 std::cin >> num2;
 std::cout << "You entered, " << num2 << "\n";
 
-//Calculates the final result adding the values entered.
-std::cout << "Result: " << num1 + num2 << "\n";
+//Asks the user to add or substract.
+int option = 0;
+std::cout << "Choose what you want to do, type 1 to add, type 2 to substract.\n";
+std::cin >> option;
 
-return 1;
+//Calculates the final result adding or substracting the inputs.
+if (option == 1){
+std::cout << "You chose addition.\n";
+std::cout << "Result of the addition: " << num1 + num2 << "\n";
+}
+else if (option == 2) {
+std::cout << "You chose substraction.\n";
+std::cout << "Result of the substraction: " << num1 - num2 << "\n";
+}
+
+return 0;
 }
